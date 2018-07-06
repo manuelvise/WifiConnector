40dfddf8d4206c36fc800e5e42243db58b1b2d0c
Manuel <manuel.visentin@crispybacon.it>
2018-07-06 12:02:33 +0200

    Fix crashes android 8

---
+++ wificonnector/src/main/java/it/manuelvise/wificonnector/interfaces/RemoveWifiListener.java
@@ -0,0 +1,10 @@
+package it.manuelvise.wificonnector.interfaces;
+
+/**
+ * Created by jflav on 10/5/2017
+ * email: jflavio90@gmail.com
+ */
+public interface RemoveWifiListener {
+    void onWifiNetworkRemoved();
+    void onWifiNetworkRemoveError();
+}
---
+++ wificonnector/src/test/java/it/manuelvise/wificonnector/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package it.manuelvise.wificonnector;
+
+import org.junit.Test;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void createWifiConnectorObjectTest(){
+        //assertNotNull(new WifiConnector());
+    }
+
+}
